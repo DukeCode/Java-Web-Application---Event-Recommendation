f90c7d933f3da2f48833ff5d237ebb44ab819a98
DukeCode <jid17@pitt.edu>
2018-04-10 02:39:40 -0400

    add my implementation of hash function

--- src/db/mysql/MySQLDBUtil.java
+++ src/db/mysql/MySQLDBUtil.java
@@ -2,7 +2,7 @@ package db.mysql;
 
 public class MySQLDBUtil {
 	private static final String HOSTNAME = "localhost";
-	private static final String PORT_NUM = "3307"; // 3306 on server and 3307 on local
+	private static final String PORT_NUM = "3306"; // 3306 on server and 3307 on local
 	public static final String DB_NAME = "db_eventRec";
 	private static final String USERNAME = "root";
 	private static final String PASSWORD = "root";
